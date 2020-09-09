5855433e03b77f2cc7ddd18fedcf0d032531c87d
Ian Gunter <iangunterofamerica@yahoo.com>
2020-09-09 08:25:27 -0600

    Add project files.

---
+++ ArrayExamples.vb
@@ -0,0 +1,27 @@
+﻿Module ArrayExamples
+
+    Sub Main()
+
+        SimpleArray()
+
+    End Sub
+
+
+    Sub SimpleArray()
+        Dim Names(5) As String
+
+
+        Names(0) = "Bilbo"
+        Names(3) = "Baggins"
+
+        Console.WriteLine("Hello " & Names(0))
+
+
+        Console.ReadLine()
+
+
+
+
+    End Sub
+
+End Module
